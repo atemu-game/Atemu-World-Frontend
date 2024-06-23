c86d78d1813bf897c9c26d77edca09d0dd502ba9
kafierence <vannam34760@gmail.com>
2024-06-23 16:39:26 +0700

    [fix] loader link

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -4,19 +4,23 @@ import React, { PropsWithChildren } from 'react';
 import Header from './Header';
 
 import Sidebar from './Sidebar';
-
+import NextTopLoader from 'nextjs-toploader';
+import { colors } from '@/themes';
 const DefaultLayout = ({ children }: PropsWithChildren) => {
   return (
-    <Flex width="full" flexDirection="column">
-      <Header />
+    <>
+      <NextTopLoader color={colors.primary[100]} showSpinner={false} />
+      <Flex width="full" flexDirection="column">
+        <Header />
 
-      <Flex width="full" position="sticky">
-        <Sidebar />
-        <Box padding={4} flex={1} width="full" as="main">
-          {children}
-        </Box>
+        <Flex width="full" position="sticky">
+          <Sidebar />
+          <Box padding={4} flex={1} width="full" as="main">
+            {children}
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
