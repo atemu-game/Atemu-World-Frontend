391d9c0e26bf6f902f7084c0d73e3d59eff65828
kafierence <vannam34760@gmail.com>
2024-07-19 11:48:07 +0700

    [fix] list sidebar

--- layouts/Sidebar/ListPage.tsx
+++ layouts/Sidebar/ListPage.tsx
@@ -38,29 +38,16 @@ const ListPage = ({ sx }: IProps) => {
       label: 'Fuel',
     },
     {
+      link: '/leaderboard',
+      icon: LeaderboardIcon,
+      label: 'Leaderboard',
+    },
+    {
       link: '/spin-of-fate',
       icon: SpinIcon,
       label: 'Spin Of Fate',
       isDisabled: true,
     },
-    {
-      link: '/leaderboard',
-      icon: LeaderboardIcon,
-      label: 'Leaderboard',
-    },
-
-    // {
-    //   link: '/incentives',
-    //   icon: IncentiveIcon,
-    //   label: 'Incentives',
-    //   isDisabled: true,
-    // },
-    // {
-    //   link: '/trade-zone',
-    //   icon: LanguageIcon,
-    //   label: 'Trade-zone',
-    //   isDisabled: true,
-    // },
   ];
   const path = usePathname();
   return (
